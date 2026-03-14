2a02139d1a069627a58dee50b82821baf17a5496
ppvan <phuclaplace@gmail.com>
2026-03-15 02:08:03 +0700

    feat: generate qr

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/ppvan/vye
 go 1.25.5
 
 require github.com/rodrigocfd/windigo v0.2.6-0.20260314115555-483b8c785ab3
+
+require (
+	github.com/piglig/go-qr v0.2.6 // indirect
+	golang.org/x/image v0.37.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"bytes"
 	_ "embed"
+	"fmt"
+	"image"
 	"runtime"
 
+	go_qr "github.com/piglig/go-qr"
 	"github.com/rodrigocfd/windigo/co"
 	"github.com/rodrigocfd/windigo/ui"
 	"github.com/rodrigocfd/windigo/win"
+	"golang.org/x/image/bmp"
 )
 
 //go:embed gopher.bmp
 var gopher []byte
 
+const title = "QR generator"
+
+// https://github.com/piglig/go-qr
+
 type MyWindow struct {
-	wnd *ui.Main
+	wnd            *ui.Main
+	textLabel      *ui.Static
+	textEdit       *ui.Edit
+	sizeLabel      *ui.Static
+	sizeOptions    *ui.ComboBox
+	generateButton *ui.Button
+	qrImage        *ui.Control
+	copyButton     *ui.Button
+	saveButton     *ui.Button
+
+	textLabelData string
+	qrImageData   []byte
 }
 
 func main() {
@@ -22,31 +42,74 @@ func main() {
 }
 
 func ShowMainWindow() int {
+
 	wnd := ui.NewMain(
 		ui.OptsMain().
-			Title("Red Window").
-			Style(co.WS_CAPTION | co.WS_SYSMENU | co.WS_CLIPCHILDREN | co.WS_BORDER | co.WS_VISIBLE | co.WS_MINIMIZEBOX | co.WS_MAXIMIZE).
-			Size(ui.Dpi(400, 300)),
+			Title(title).
+			Size(ui.Dpi(720, 480)),
 	)
 
-	me := &MyWindow{wnd}
+	textLabel := ui.NewStatic(wnd, ui.OptsStatic().
+		Text("Text").
+		Position(ui.Dpi(20, 20)),
+	)
+
+	textEdit := ui.NewEdit(wnd, ui.OptsEdit().
+		Position(ui.Dpi(20, 40)).
+		Width(ui.DpiX(320)),
+	)
+
+	sizeLabel := ui.NewStatic(wnd, ui.OptsStatic().
+		Text("Size").
+		Position(ui.Dpi(20, 215)),
+	)
+
+	sizeOptions := ui.NewComboBox(wnd, ui.OptsComboBox().
+		Position(ui.Dpi(20, 235)).
+		Width(ui.DpiX(320)).
+		Texts("Auto").
+		Select(0),
+	)
+
+	generateButton := ui.NewButton(wnd, ui.OptsButton().
+		Position(ui.Dpi(20, 430)).
+		Text("Generate").
+		Height(ui.DpiY(30)).
+		Width(ui.DpiX(320)),
+	)
+
+	qrImage := ui.NewControl(wnd, ui.OptsControl().Position(ui.Dpi(380, 20)).Size(ui.Dpi(320, 320)))
+
+	me := &MyWindow{
+		wnd:            wnd,
+		textLabel:      textLabel,
+		textEdit:       textEdit,
+		sizeLabel:      sizeLabel,
+		sizeOptions:    sizeOptions,
+		generateButton: generateButton,
+		qrImage:        qrImage,
+	}
 	me.events()
 	return wnd.RunAsMain()
 }
 
 func (me *MyWindow) events() {
-	me.wnd.On().WmPaint(func() {
+	me.qrImage.On().WmPaint(func() {
+		if me.qrImageData == nil {
+			return
+		}
+
 		var ps win.PAINTSTRUCT
-		hdc, err := me.wnd.Hwnd().BeginPaint(&ps)
+		hdc, err := me.qrImage.Hwnd().BeginPaint(&ps)
 		if err != nil {
 			panic(err)
 		}
-		defer me.wnd.Hwnd().EndPaint(&ps)
+		defer me.qrImage.Hwnd().EndPaint(&ps)
 
 		rel := win.NewOleReleaser()
 		defer rel.Release() // important: release your COM resources to avoid leaks
 
-		stream, err := win.SHCreateMemStream(rel, gopher)
+		stream, err := win.SHCreateMemStream(rel, me.qrImageData)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -68,4 +131,45 @@ func (me *MyWindow) events() {
 			win.SIZE{Cx: sz.Cx, Cy: -sz.Cy},
 		)
 	})
+
+	me.generateButton.On().BnClicked(func() {
+		text := me.textEdit.Text()
+		errCorLvl := go_qr.Low
+		qr, err := go_qr.EncodeText(text, errCorLvl)
+		if err != nil {
+			panic(err)
+		}
+		config := go_qr.NewQrCodeImgConfig(10, 4)
+
+		var buf bytes.Buffer
+		qr.WriteAsPNG(config, &buf)
+
+		bitmapBuf, _ := pngToBitmapInMemory(buf.Bytes())
+
+		me.qrImageData = bitmapBuf
+	})
+}
+
+func pngToBitmapInMemory(pngData []byte) ([]byte, error) {
+	// Use bytes.NewReader to create an io.Reader from the input PNG byte slice
+	pngReader := bytes.NewReader(pngData)
+
+	// 1. Decode the PNG image data into a generic image.Image interface
+	img, _, err := image.Decode(pngReader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode PNG: %w", err)
+	}
+
+	// Create a bytes.Buffer to store the encoded BMP data in memory
+	var bmpBuffer bytes.Buffer
+
+	// 2. Encode the image.Image into BMP format, writing to the buffer
+	// Note: The golang.org/x/image/bmp package only supports specific bit depths.
+	err = bmp.Encode(&bmpBuffer, img)
+	if err != nil {
+		return nil, fmt.Errorf("failed to encode BMP: %w", err)
+	}
+
+	// Return the in-memory BMP byte slice
+	return bmpBuffer.Bytes(), nil
 }
